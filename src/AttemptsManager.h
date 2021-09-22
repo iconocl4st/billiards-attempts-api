bb959b70e41fccd51bde7050a2d08e7fc6a6f7ee
iconocl4st <you@example.com>
2021-09-22 17:57:40 -0600

    a

---
+++ src/AttemptsManager.h
@@ -0,0 +1,19 @@
+//
+// Created by thallock on 9/21/21.
+//
+
+#ifndef IDEA_ATTEMPTSMANAGER_H
+#define IDEA_ATTEMPTSMANAGER_H
+
+#include "common/attempts/ShotAttempt.h"
+
+namespace billiards::attempts {
+	class AttemptsManager {
+	public:
+
+	};
+}
+
+
+
+#endif //IDEA_ATTEMPTSMANAGER_H
